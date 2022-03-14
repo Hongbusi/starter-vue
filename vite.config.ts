3372f33ec338e1d109999d9e9240b56b16d0543e
Hongbusi <q951686662@qq.com>
2022-03-14 15:35:18 +0800

    chore: update

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import path from 'path'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite'
 
 export default defineConfig({
   resolve: {
@@ -19,6 +20,13 @@ export default defineConfig({
         'vue'
       ],
       dts: 'src/auto-imports.d.ts'
+    }),
+
+    // https://github.com/antfu/unplugin-vue-components
+    Components({
+      extensions: ['vue'],
+      include: [/\.vue$/, /\.vue\?vue/],
+      dts: 'src/components.d.ts'
     })
   ]
 })
