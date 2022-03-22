aaec957486bfc438728959ba511e5998f33a11d7
Hongbusi <q951686662@qq.com>
2022-03-22 17:03:19 +0800

    feat: page

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,14 @@
 import { ViteSSG } from 'vite-ssg'
+import generatedRoutes from 'virtual:generated-pages'
+import { setupLayouts } from 'virtual:generated-layouts'
+
 import App from './App.vue'
 
 import '@unocss/reset/tailwind.css'
 import './styles/main.css'
 import 'uno.css'
 
-import routes from './router'
+const routes = setupLayouts(generatedRoutes)
 
 export const createApp = ViteSSG(
   App,
--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,8 @@
 import path from 'path'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import Pages from 'vite-plugin-pages'
+import Layouts from 'vite-plugin-vue-layouts'
 import AutoImport from 'unplugin-auto-import/vite'
 import Components from 'unplugin-vue-components/vite'
 import Unocss from 'unocss/vite'
@@ -14,6 +16,14 @@ export default defineConfig({
 
   plugins: [
     vue(),
+
+    // https://github.com/hannoeru/vite-plugin-pages
+    Pages({
+      extensions: ['vue', 'md']
+    }),
+
+    // https://github.com/JohnCampionJr/vite-plugin-vue-layouts
+    Layouts(),
 
     // https://github.com/antfu/unplugin-auto-import
     AutoImport({
