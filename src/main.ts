1e4ffc9ceb89f2b2abbe1df2e41f11a52bdc9e41
Hongbusi <q951686662@qq.com>
2022-03-22 16:45:01 +0800

    feat: nprogress

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,15 @@ import { ViteSSG } from 'vite-ssg'
 import App from './App.vue'
 
 import '@unocss/reset/tailwind.css'
+import './styles/main.css'
 import 'uno.css'
 
 import routes from './router'
 
-export const createApp = ViteSSG(App, { routes }, () => {
-  // install plugins etc.
-})
+export const createApp = ViteSSG(
+  App,
+  { routes, base: import.meta.env.BASE_URL },
+  (ctx) => {
+    Object.values(import.meta.globEager('./modules/*.ts')).forEach(i => i.install?.(ctx))
+  }
+)
